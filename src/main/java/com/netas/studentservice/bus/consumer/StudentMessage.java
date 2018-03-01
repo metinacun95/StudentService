85a018ff5c0262c5e5e807bc1920bcb5c5cc2615
Metin ACUN <metinacun95@gmail.com>
2018-03-01 16:11:06 +0300

    deneme

--- src/main/java/com/netas/studentservice/bus/consumer/StudentMessage.java
+++ src/main/java/com/netas/studentservice/bus/consumer/StudentMessage.java
@@ -3,10 +3,11 @@ package com.netas.studentservice.bus.consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-
+import org.springframework.stereotype.Component;
+@Component
 public class StudentMessage {
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-
+  
 	@KafkaListener(topics="${kafka.topic.consumer}")
 	public void listen(String message) {
 		LOGGER.debug("[getMessage] : Message -> {}", message);
